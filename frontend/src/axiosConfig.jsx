b4b652ef7305d4ccba6f66b42bb00eeb6edc6241
EunseShimElsie <simeunse04@gmail.com>
2025-08-17 17:28:18 +1000

    fix route

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.107.56.66:5001', // live
+  baseURL: 'http://13.210.117.196:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
