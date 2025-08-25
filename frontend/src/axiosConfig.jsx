0d464056414e61d02e0373700b69cc3571ed6a75
EunseShimElsie <simeunse04@gmail.com>
2025-08-25 15:05:37 +1000

    BTA-7 demo

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://13.210.117.196:5001', // live
+  //baseURL: 'http://54.252.157.1:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
